7d02153e42f0f63c73cfd41c5cf61d6a83bf460a
Kyle Topasna <kyle.topasna@gmail.com>
2022-12-19 09:41:41 -0900

    Add files via upload

---
+++ Dot NET TCP Shell/TCP_POC.ps1
@@ -0,0 +1,22 @@
+﻿# Get System IP Address
+$ip_address = Get-NetIPAddress -AddressFamily IPv4 | Where-Object {$_.IPAddress -notlike "169.254*"} | Select-Object IPAddress -First 1
+$ip_address = $ip_address.IPAddress
+
+# Start TCP Server
+$serverProcess = Start-Process .\tcp_server.exe $ip_address -PassThru
+
+# Wait for the process to start
+Start-Sleep -Seconds 3
+
+# Start the TCP Client program
+$clientProcess = Start-Process .\TCP_Reverse_Shell.exe $ip_address -PassThru
+
+# Wait for the process to start
+Start-Sleep -Seconds 3
+
+# Sleep so user can enter a command
+start-sleep 10
+
+# Stop running processes
+Stop-Process -id $serverProcess.Id
+Stop-Process -id $clientProcess.Id
